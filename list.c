bdadc44e57be6af46d73c131a7ccec7fae75e4fc
alex <819685619@qq.com>
2017-06-06 14:17:01 +0800

    fix bugs

--- list.c
+++ list.c
@@ -4,10 +4,7 @@
 #include "list.h"
 
 
-typedef struct LIST{
-	_Node *head;
-	int size;
-}List;
+
 
 
 _Node *list_begin(List *list)
@@ -63,51 +60,38 @@ int list_empty(List *list)
 }
 
 
-List *create_list()
+int create_list(List *list, void (*freefn)(void *p))
 {
-	List *list = (List *)malloc(sizeof(List));
 	list->head = (_Node *)malloc(sizeof(_Node));
 	list->head->next = list->head;
 	list->head->prev = list->head;
 	list->size = 0;
-	return list;
-}
-//don't free memory ,user should free
-int delete_list(List **list)
-{
-    if(*list == 0)
-		return 0;
-	_Node *head = (*list)->head;
-	//while(head->next != head){
-	//	_Node *first = head->next;
-	//	head->next = first->next;
-	//	free(first);
-	//}
-	free(head);
-	free((*list));
-	(*list) = 0;
+	list->freefn = freefn;
 	return SUCCESS;
 }
 
-int delete_list_free(List **list)
+
+int delete_list(List *list)
 {
-    if((*list) == 0)
+        if(list == NULL)
 		return 0;
-	_Node *head = (*list)->head;
+	_Node *head = list->head;
 	while(head->next != head){
 		_Node *first = head->next;
 		head->next = first->next;
-		free(first);
+		if(list->freefn != NULL)
+			list->freefn(first);//free _Node.data
+		free(first);//free _Node
 	}
 	free(head);
-	free((*list));
-	(*list) = 0;
+	list->head = NULL;
+	list->size = 0;
 	return SUCCESS;
 }
 
 void list_clear(List *list)
 {
-    if(list == 0)
+    if(list == NULL)
 		return ;
     _Node *head = list->head;
 	list->size = 0;
@@ -117,7 +101,7 @@ void list_clear(List *list)
 
 void list_push_back(List *list, _Node *node)
 {
-    if(list == 0 || node == 0)
+    if(list == NULL || node == NULL)
 		return ;
 	_Node *head = list->head;
 	node->next = head;
@@ -129,7 +113,7 @@ void list_push_back(List *list, _Node *node)
 
 void list_push_front(List *list, _Node *node)
 {
-    if(list == 0 || node == 0)
+    if(list == NULL || node == NULL)
 		return ;
     _Node *head = list->head;
     node->next = head->next;
@@ -161,7 +145,7 @@ _Node *list_pop_front(List *list)
 
 int list_insert_after(List *list, _Node *index, _Node *node)
 {
-    if(!list|| !node|| !index)
+        if(list == NULL || node == NULL || index == NULL )
 		return FAILURE;
 	node->next = index->next;
 	node->prev = index;
@@ -179,7 +163,7 @@ int list_insert_front(List *list, _Node *index, _Node *node)
 }
 int list_delete(List *list, _Node *node)
 {
-    if(!list || !node)
+        if(list == NULL || node == NULL)
 		return FAILURE;
 	_Node *prev = node->prev;
 	_Node *next = node->next;
@@ -208,10 +192,8 @@ _Node *list_search(List *list, int condition(_Node *node, void *argc), void *arg
 
 void list_merge(List *list1, List *list2, int (*compare)(_Node *p, _Node *q))
 {
-    if(list1 == NULL || list1->size == 0 ||
-       list2 == NULL || list2->size == 0)
+    if(list1 == NULL || list2 == NULL)
         return ;
-    list1->size += list_size(list2);
 
     _Node *first1 = list_begin(list1);
     _Node *last1 = list_end(list1);
@@ -222,7 +204,6 @@ void list_merge(List *list1, List *list2, int (*compare)(_Node *p, _Node *q))
             _Node *temp = first2->next;
             list_insert_front(list1, first1, first2);
             first2 = temp;
-            first1 = first1->next;
         }
         else{
             first1 = first1->next;
@@ -237,39 +218,52 @@ void list_merge(List *list1, List *list2, int (*compare)(_Node *p, _Node *q))
     list_clear(list2);
 }
 
-void list_swap(List **list1, List **list2)
+void list_swap(List *list1, List *list2)
 {
-    List *temp = *list2;
-    *list2 = *list1;
-    *list1 = temp;
+    
+    void (*tfreefn)(void *p) = list1->freefn;
+    _Node *thead = list1->head;
+    int tsize = list1->size;
+
+    list1->freefn = list2->freefn;
+    list1->head = list2->head;
+    list1->size = list2->size;
+
+    list2->freefn = tfreefn;
+    list2->head = thead;
+    list2->size = tsize;
 }
 
-void list_sort(List **plist, int (*compare)(_Node *p, _Node *q))
+void list_sort(List *list, int (*compare)(_Node *p, _Node *q))
 {
-    List *list = *plist;
 	if(list->size == 0 || list->size == 1)
 		return ;
 	int index;
-	List *carry = create_list();
-	List *counter[64];
+	//init carry
+	List carry;
+	create_list(&carry, list->freefn);
+	//init counter
+	List counter[64];
 	for(index=0;index<64;index++)
-        counter[index] = create_list();
+        	create_list(&counter[index], list->freefn);
+	//bottom to up merge sort
 	int fill = 0;
 	while(!list_empty(list)){
-        list_push_back(carry, list_pop_front(list));
-        int i=0;
-        while(i < fill && !list_empty(counter[i])){
-            list_merge(counter[i], carry, compare);
-            list_swap(&carry, &counter[i]);
-            i++;
-        }
-        list_swap(&counter[i], &carry);
-        if(i == fill) ++fill;
+		list_push_back(&carry, list_pop_front(list));
+		int i=0;
+		while(i < fill && !list_empty(&counter[i])){
+		    list_merge(&counter[i], &carry, compare);
+		    list_swap(&carry, &counter[i]);
+		    i++;
+		}
+		list_swap(&counter[i], &carry);
+		if(i == fill) ++fill;
 	}
 	for(index = 1; index<fill; index++)
-        list_merge(counter[index], counter[index-1], compare);
-	list_swap(plist, &counter[fill-1]);
+        	list_merge(&counter[index], &counter[index-1], compare);
+	list_swap(list, &counter[fill-1]);
+	//delete counter and carry
 	for(index=0;index<64;index++)
-        delete_list(&counter[index]);
-    delete_list(&carry);
+       		delete_list(&counter[index]);
+    	delete_list(&carry);
 }
--- list.h
+++ list.h
@@ -7,18 +7,26 @@ typedef struct _NODE{
 	struct _NODE *next;
 }_Node;
 
-typedef struct LIST List;
+//typedef struct LIST List;
+
+
+typedef struct LIST{
+	void (*freefn)(void *p);
+	_Node *head;
+	int size;
+}List;
+
 
 #define SUCCESS 1
 #define FAILURE 0
 
-List *create_list();
-int delete_list(List **list);
-int delete_list_free(List **list);
+int create_list(List *list, void (*freefn)(void *p));
+int delete_list(List *list);
+
 
 void list_clear(List *list);
 
-void list_swap(List **list1, List **list2);
+void list_swap(List *list1, List *list2);
 
 int list_insert_after(List *list, _Node *index, _Node *node);
 int list_insert_front(List *list, _Node *index, _Node *node);
@@ -41,5 +49,5 @@ void list_push_front(List *list, _Node *node);
 void list_push_back(List *list, _Node *node);
 
 void list_merge(List *list1, List *list2, int (*compare)(_Node *p, _Node *q));
-void list_sort(List **list, int (*compare)(_Node *p, _Node *q));
+void list_sort(List *list, int (*compare)(_Node *p, _Node *q));
 #endif
